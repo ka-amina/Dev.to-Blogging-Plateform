cb7c32cac7e27e5096ae9f5f09561b84e2d383f0
ka-amina <aminakara400@gmail.com>
2025-01-03 06:30:15 +0100

    DT-18: Ajouter la fonctionalité d'affichier les tags

---
+++ app/Models/Tag.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Models;
+
+use App\Models\ORM;
+
+class Tag {
+    protected $id;
+    protected $name;
+    protected $table='tags';
+    protected $orm;
+
+    public function __construct() {
+        $this->orm= new ORM();
+        $this->orm->setTable($this->table);
+    }
+
+    public function setTagName($name){
+        $this->name=$name;
+    }
+    public function getTagName(){
+        return $this-> name;
+    } 
+
+    public function  getTags(){
+        return $this->orm->read();
+    }
+
+}
+
+
+?>
--- view/home.php
+++ view/home.php
@@ -3,13 +3,19 @@ require_once '../app/config/connexion.php';
 require '../vendor/autoload.php';
 
 use App\Controllers\CategoryController;
+use App\Controllers\TagController;
 
 
 $controller = new CategoryController();
+$Tag=new TagController;
 
 // $categories = $controller->listCategories();
 // print_r($categories);
 
 // $controller->deleteCategory(['id' => 2]);
 // $controller->createCategory(['name'=> 'cyber security']);
-$controller->updateCategory(['name'=>'test'],['id'=>6]) ;
+// $controller->updateCategory(['name'=>'test'],['id'=>6]) ;
+
+// tags
+$tags= $Tag->listTags();
+print_r($tags);
